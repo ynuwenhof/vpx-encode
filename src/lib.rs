ea7973bc09f16403c82a32a79464b96c38fa9e7a
Andrew Straw <strawman@astraw.com>
2019-06-09 08:25:26 +0200

    add docstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//! Rust interface to libvpx encoder
+//!
+//! This crate provides a Rust API to use
+//! [libvpx](https://en.wikipedia.org/wiki/Libvpx) for encoding images.
+//!
+//! It it based entirely on code from [srs](https://crates.io/crates/srs).
+//! Compared to the original `srs`, this code has been simplified for use as a
+//! library and updated to add support for both the VP8 codec and (optionally)
+//! the VP9 codec.
+//!
+//! # Optional features
+//!
+//! Compile with the cargo feature `vp9` to enable support for the VP9 codec.
+//!
+//! # Example
+//!
+//! An example of using `vpx-encode` can be found in the [`record-screen`]()
+//! program. The source code for `record-screen` is in the [vpx-encode git
+//! repository]().
+//!
+//! # Contributing
+//!
+//! All contributions are appreciated.
+
 // vpx_sys is provided by the `env-libvpx-sys` crate
 
 use std::os::raw::{c_int, c_uint, c_ulong};
